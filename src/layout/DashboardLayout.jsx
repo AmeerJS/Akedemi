ffa8a740528ed10024799b15a3402ed4c152ccb6
devAmeer <ameerabro004@gmail.com>
2025-07-25 16:44:38 +0500

    Dashboard Complete

--- src/layout/DashboardLayout.jsx
+++ src/layout/DashboardLayout.jsx
@@ -6,13 +6,10 @@ export default function DashboardLayout({ children }) {
   return (
     <div className="flex min-h-screen bg-gray-100">
       <Sidebar />
-
       <div className="flex-1 flex flex-col">
       <Navbar />
-
       <main className="flex-1 p-4">{children}</main>
       </div>
-      
       <RightPanel />
     </div>
   );
